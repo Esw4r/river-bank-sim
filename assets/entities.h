8f3c9388a6df4c87de5d104af0415237ce257337
s4nj1th <sanjith@tutamail.com>
2025-02-09 22:37:24 +0530

    first commit

---
+++ assets/entities.h
@@ -0,0 +1,98 @@
+#include <string>
+
+typedef unsigned char uint8;
+using namespace std;
+
+class Animal {
+private:
+  string name;
+  uint8 health;
+  uint8 age;
+  uint8 expectancy;
+  bool gender;
+  uint8 hunger;
+  uint8 thirst;
+  uint8 speed;
+  uint8 vision;
+  uint8 direction[2];
+  uint8 eyes[2];
+  uint8 ears[2];
+  uint8 limbs[4];
+  uint8 head;
+  uint8 torso;
+  uint8 genitals;
+  uint8 fov;
+  uint8 posn[2];
+  uint8 fear;
+
+public:
+  Animal() {}
+
+  uint8 getHealth() { return health; }
+  void setHealth(uint8 x) { health = x; }
+
+  uint8 getAge() { return age; }
+  void setAge(uint8 x) { age = x; }
+
+  uint8 getExp() { return expectancy; }
+  void setExp(uint8 x) { expectancy = x; }
+
+  uint8 getHunger() { return hunger; }
+  void setHunger(uint8 x) { hunger = x; }
+
+  uint8 getThirst() { return thirst; }
+  void setThirst(uint8 x) { thirst = x; }
+
+  uint8 getSpeed() { return speed; }
+  void setSpeed(uint8 x) { speed = x; }
+
+  uint8 getVision() { return vision; }
+  void setVision(uint8 x) { vision = x; }
+
+  uint8 *getEars() { return ears; }
+  void setEars(uint8 x, uint8 y) {
+    ears[0] = x;
+    ears[1] = y;
+  }
+
+  uint8 *getEyes() { return eyes; }
+  void setEyes(uint8 x, uint8 y) {
+    eyes[0] = x;
+    eyes[1] = y;
+  }
+
+  uint8 *getDir() { return direction; }
+  void setDirection(uint8 x, uint8 y) {
+    direction[0] = x;
+    direction[1] = y;
+  }
+
+  uint8 *getLimbs() { return limbs; }
+  void setLimbs(uint8 w, uint8 x, uint8 y, uint8 z) {
+    limbs[0] = w;
+    limbs[1] = x;
+    limbs[2] = y;
+    limbs[3] = z;
+  }
+
+  uint8 getHead() { return head; }
+  void setHead(uint8 x) { head = x; }
+
+  uint8 getTorso() { return torso; }
+  void setTorso(uint8 x) { torso = x; }
+
+  uint8 getGenitals() { return genitals; }
+  void setGenitals(uint8 x) { genitals = x; }
+
+  uint8 getFov() { return fov; }
+  void setFov(uint8 x) { fov = x; }
+};
+
+class Bison : public Animal {
+private:
+  uint8 herdTend;
+  uint8 strength;
+
+public:
+  void search() {}
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+#include "assets/entities.h"
+#include "assets/map.h"
+
+int main() {
+
+}
